0d257db84b2a7a939c4d2dc174c458388e108f1c
b-cancel <bryan.o.cancel@gmail.com>
2017-02-05 22:30:15 -0600

    selection tool almost finsihed, I need to find a better casting method from array list to an array... then I need to test if the lasso tool actually selects things

---
+++ 2DInGameGameObjectSelectionTool/Assets/Scripts/cameraManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraManager : MonoBehaviour {
+
+    GameObject selectionToolGO;
+
+	// Use this for initialization
+	void Start () {
+        selectionToolGO = GameObject.Find("gameManager").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Material mat;
+
+    void OnPostRender()
+    {
+        if (selectionToolGO.GetComponent<selectionTool>().lassoTool == true)
+        {
+            ArrayList points = selectionToolGO.GetComponent<selectionTool>().ourPoints;
+
+            mat.SetPass(0);
+
+            GL.Begin(GL.LINES);
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                Vector3 start;
+                Vector3 end;
+
+                if (i == 0)
+                {
+                    start = (Vector3)points[points.Count - 1]; //last point to...
+                    end = (Vector3)points[i]; //first
+                }
+                else
+                {
+                    start = (Vector3)points[i - 1];
+                    end = (Vector3)points[i];
+                }
+
+                GL.Vertex3(start.x, start.y, start.z);
+                GL.Vertex3(end.x, end.y, end.z);
+            }
+
+            GL.End();
+        }
+    }
+}
--- 2DInGameGameObjectSelectionTool/Assets/Scripts/selectionTool.cs
+++ 2DInGameGameObjectSelectionTool/Assets/Scripts/selectionTool.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class selectionTool : MonoBehaviour {
 
@@ -8,14 +9,7 @@ public class selectionTool : MonoBehaviour {
     //1. LEFT CLICKS
     //2. LEFT CTRL KEY (as modifier)
 
-    //I am using in Large Part Code From Here http://hyunkell.com/blog/rts-style-unit-selection-in-unity-5/
-
     ArrayList selectedObjects; //this will store all of our selected object
-
-	// Use this for initialization
-	void Start () {
-        selectedObjects = new ArrayList();
-	}
 
     //used to create the Rectangle every Frame with onGUI
     //used to finish the Lasso Selection if it isnt done on its own
@@ -25,11 +19,26 @@ public class selectionTool : MonoBehaviour {
     Vector3 finalWorldMousePos;
 
     //these indicate which tool is being used when you are clicking the button
-    bool rectTool;
-    bool lassoTool;
+    public bool rectTool;
+    public bool lassoTool;
+    public ArrayList ourPoints;
+
+    // Use this for initialization
+    void Start () {
+        selectedObjects = new ArrayList();
+        rectTool = false;
+        lassoTool = false;
+        ourPoints = new ArrayList();
+    }
+
+    //the two variables we use to not save points with the lasso tool every single frame (rather we save the point every couple of seconds)
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
 
     // Update is called once per frame
     void Update () {
+
+        //--- Taking Care of what happens when we click on our mouse
 
         Vector3 screenMousePos;
         Vector3 worldMousePos;
@@ -52,60 +61,60 @@ public class selectionTool : MonoBehaviour {
 
             if (hit.collider != null) //we are over a gameobject...
             {
+
+                Debug.Log("Over a GO");
+
                 //the gameObject we have selected
                 GameObject newGO = hit.collider.gameObject;
 
-                if (selectedObjects.Count != 0) //if our array list is NOT empty we might have to delete objects
+                if (!selectedObjects.Contains(newGO))//we want to add the object to our selection group
                 {
-                    GameObject old = (GameObject)selectedObjects[0];
-
-                    if (old.name != newGO.name) //if we have selected a different TYPE of game object then delete it
+                    if (selectedObjects.Count != 0) //if our array list is NOT empty we might have to delete objects
                     {
-                        ///TODO MAYBE... ask user whether they (REQUIRES SIGNIFICANT REWRITING OF CODE)
-                        //1. Want to Select THIS... and DE select OTHER
-                        //2. Want to DeSelect THIS...
-
-                        foreach (GameObject go in selectedObjects)
+                        if (!Input.GetKey(KeyCode.LeftControl)) //we dont want to multiple select
                         {
-                            //unhighlight the game object
-                            go.GetComponent<SpriteOutline>().DeActivateOutline();
+                            deselectAll();
                         }
+                        else //we want to MULTIPLE select
+                        {
+                            //NOTE: we want to maintain a selectedList of objects of the same type
+                            GameObject old = (GameObject)selectedObjects[0];
 
-                        selectedObjects.Clear();
+                            //NOTE: I defined type by name(works well with programatically cloned objects)... feel free to define type by label or layer... etc...
+                            //if we have selected a different TYPE of game object then delete all the previous objects in the list
+                            if (old.tag != newGO.tag)
+                            {
+                                ///TODO MAYBE... ask user whether they (REQUIRES SIGNIFICANT REWRITING OF CODE)
+                                //1. Want to Select THIS... and DE select OTHERS
+                                //2. or Keep OTHERS
+                                deselectAll();
+                            }
+                        }
                     }
-                    //else we simply need to add the game object
+                    //ELSE this is our first selection
                 }
-                else
-                {
-                    //else we are switching to the selection tool and must therefore shut off all other tools that cause Conflicts
-                }
+                //ELSE we simply deselect it...
 
-                //we haven't selected this game object before... then select
-                if (!selectedObjects.Contains(newGO))
-                {
-                    Debug.Log("Object Being Selected");
-                    selectedObjects.Add(newGO);
-                    newGO.GetComponent<SpriteOutline>().ActivateOutline();
-                }
-                else
-                {
-                    //else... deselect
-                    selectedObjects.Remove(newGO);
-                    newGO.GetComponent<SpriteOutline>().DeActivateOutline();
-                }
-
+                selectOrDeselectDepends(newGO);
             }
             else
             {
-                //we want to make a selection box or a Lasso... 
+                //we want to make a selection box or a Lasso... this is taken care off by tracking the pointer below
 
                 if (Input.GetKey(KeyCode.LeftControl))//if we are pressing control then Lasso
                 {
                     lassoTool = true;
+                    ourPoints.Add(initialWorldMousePos);
                 }
-                else //simply make a selection box
+                else if(Input.GetKey(KeyCode.LeftAlt))//simply make a selection box
                 {
                     rectTool = true;
+                }
+                else
+                {
+                    Debug.Log(selectedObjects.Count);
+                    deselectAll();
+                    Debug.Log(selectedObjects.Count);
                 }
             }
         }
@@ -126,6 +135,13 @@ public class selectionTool : MonoBehaviour {
                 if (lassoTool == true)
                 {
                     Debug.Log("Lasso Tool");
+
+                    //add this particular point every couple nextUpdate time
+                    if (Time.time > nextActionTime)
+                    {
+                        nextActionTime = Time.time + period;
+                        ourPoints.Add(finalWorldMousePos);
+                    }
                 }
                 //ELSE for the Rectangle Tool We dont need to keep track of anything except what we are already keeping track of
             }
@@ -137,7 +153,6 @@ public class selectionTool : MonoBehaviour {
                 {
                     Debug.Log("Rect Tool CLOSE");
 
-                    //code
                     //NOTE: the rectangle is built from the top left corner (topLeftCorner Coords, size.x, size.y)
                     Vector3 init = initialScreenMousePos;
                     Vector3 final = finalScreenMousePos;
@@ -154,9 +169,12 @@ public class selectionTool : MonoBehaviour {
                     float halfHeight = (largeY - smallY)/2;
                     Vector2 centerPoint = Camera.main.WorldToViewportPoint(new Vector3(halfWidth, halfHeight));
 
-                    Collider2D[] inRect = Physics2D.OverlapBoxAll(new Vector2(centerPoint.x,centerPoint.y),new Vector2(halfWidth,halfHeight),0);
-
+                    //Using OverlapArea
+                    Collider2D[] inRect = Physics2D.OverlapAreaAll(initialWorldMousePos, finalWorldMousePos);
                     Debug.Log("----------Collisions:" + inRect.Length);
+
+                    //select all dogs in Rectangle Area
+                    selectAll(inRect);
 
                     rectTool = false;
                 }
@@ -164,7 +182,15 @@ public class selectionTool : MonoBehaviour {
                 {
                     Debug.Log("Lasso Tool CLOSE");
 
-                    //code
+                    //create polygonal collider
+                    this.gameObject.AddComponent<PolygonCollider2D>();
+                    Vector2[] v2Arr = Array.ConvertAll(ourPoints.ToArray(), item => (Vector2)item); //TODO this casting isnt working... -_-
+                    GetComponent<PolygonCollider2D>().SetPath(0, v2Arr);
+                    GetComponent<PolygonCollider2D>().isTrigger = true;
+
+                    runCounter = 1; //indicate this to our fixedUpdate
+
+                    ourPoints.Clear();
 
                     lassoTool = false;
                 }
@@ -172,10 +198,84 @@ public class selectionTool : MonoBehaviour {
         } 
     }
 
+    //---helper Functions for Update Function
+
+    //this function SELECTS an object IF it hasn't been selected
+    //and DE-SELECTS an object IF its has been selected
+    void selectOrDeselectDepends(GameObject GO)
+    {
+        if (selectedObjects.Contains(GO)) //if selected -> DE-SELECT
+        {
+            selectedObjects.Remove(GO.gameObject);
+            GO.gameObject.GetComponent<SpriteOutline>().DeActivateOutline();
+        }
+        else //if NOT selected -> SELECT
+        {
+            selectedObjects.Add(GO.gameObject);
+            GO.gameObject.GetComponent<SpriteOutline>().ActivateOutline();
+        }
+    }
+
+    void deselectAll()
+    {
+        for(int i=0; i<selectedObjects.Count; i++)
+            ((GameObject)selectedObjects[i]).GetComponent<SpriteOutline>().DeActivateOutline();
+        selectedObjects.Clear();
+    }
+
+    void selectAll(ArrayList colliders)
+    {
+        selectAll(Array.ConvertAll(colliders.ToArray(), item => (Collider2D)item)); //TODO this casting isnt working... -_-
+    }
+
+    void selectAll(Collider2D[] colliders)
+    {
+        foreach (Collider2D col in colliders)
+        {
+            (col.gameObject).GetComponent<SpriteOutline>().ActivateOutline();
+            selectedObjects.Add(col.gameObject);
+        }
+    }
+
+    //---These are needed for our polygon collider to do its job after the Lasso Tool Has been Let Go
+
+    /*
+     * 1. Update Function create the 2dpolygoncollider/trigger on the indicated points
+     * 2. fixed update runs
+     * 3. onTriggerEnter runs (hopefully as many times as the collisions in it)
+     * 4. fixed Update runs.... handle all the collisions found and select them... now the collider isnt needed so its deleted
+     */
+    int runCounter = 0; //if 0 nothing happens... if 1 we have created the collider but Not yet saved collisions... if 2 we must select collisions and delete collider
+    ArrayList collider2DArr; //keeps track of all the collisions found in or touching our polygon collider
+
+    void FixedUpdate()
+    {
+        if (runCounter != 0)
+        {
+            if (runCounter == 2)
+            {
+                //select Collisions in Lasso
+                selectAll(collider2DArr);
+
+                //delete the polygon Collider
+                Destroy(GetComponent<PolygonCollider2D>());
+
+                runCounter = 0; //we are back at our regular state
+            }
+            //ELSE we need to wait for onTriggerEnter to run and add our Collisions to the Collider2D list
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Thing in Lasso");
+        collider2DArr.Add(other);
+    }
+
+    //---OnGUI is used to draw the visible Rectangle When using the Rect Selection Tool
 
     void OnGUI()
     {
-        Debug.Log("rect: " + rectTool);
         if (rectTool == true)
         {
             //NOTE: the rectangle is built from the top left corner (topLeftCorner Coords, size.x, size.y)
@@ -194,13 +294,13 @@ public class selectionTool : MonoBehaviour {
             DrawScreenRect(new Rect(smallX, smallY, largeX - smallX , largeY - smallY), new Color(0.8f, 0.8f, 0.95f, 0.25f));
             DrawScreenRectBorder(new Rect(smallX, smallY, largeX - smallX, largeY - smallY), 2, new Color(0.8f, 0.8f, 0.95f));
         }
-        else if(lassoTool == true)
-        {
-
-        }
+        //NOTE: the Lines for the Lasso Tool are Taken care of in the cameraManager Script
     }
 
     //---Drawing Tools
+
+    //Taken From http://hyunkell.com/blog/rts-style-unit-selection-in-unity-5/
+    //and https://forum.unity3d.com/threads/draw-a-simple-rectangle-filled-with-a-color.116348/
 
     private static Texture2D _staticRectTexture;
     private static GUIStyle _staticRectStyle;
